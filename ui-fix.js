ae9f63294d1309c0158a66a393aa224afa2e6234
alex-sforza <alex.whysoidiot.ss@gmail.com>
2026-08-30 15:03:11 +0300

    Load natural portrait transitions

--- ui-fix.js
+++ ui-fix.js
@@ -4,7 +4,7 @@
   function load(src){
     return new Promise(function(resolve,reject){
       var s=document.createElement('script');
-      s.src=src+'?v=20260830-2';
+      s.src=src+'?v=20260830-3';
       s.onload=resolve;
       s.onerror=reject;
       document.head.appendChild(s);
@@ -12,6 +12,7 @@
   }
   load('ui-fix-core.js')
     .then(function(){return load('portrait-variations.js');})
+    .then(function(){return load('portrait-transition-fix.js');})
     .then(function(){return load('presentation-fix.js');})
     .catch(function(err){console.error('UI layer load failed',err);});
 })();
