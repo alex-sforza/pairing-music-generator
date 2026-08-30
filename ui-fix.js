28f4d0dfacec661a2903b7a9521e1ad78aaa7f64
alex-sforza <alex.whysoidiot.ss@gmail.com>
2026-08-30 16:11:38 +0300

    style: simplify pairing result header and facts

--- ui-fix.js
+++ ui-fix.js
@@ -1,6 +1,9 @@
 /* Stable loader: one UI layer, one source of truth. */
 (function(){
   'use strict';
+  var style=document.createElement('style');
+  style.textContent='.pair-names{display:none!important}.facts .fact:nth-child(2),.facts .fact:nth-child(3){display:none!important}';
+  document.head.appendChild(style);
   var s=document.createElement('script');
   s.src='ui-fix-core.js?v=20260830-stable';
   s.onload=function(){console.info('Pairing Music Generator UI loaded');};
